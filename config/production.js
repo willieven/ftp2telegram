7d80d56bbea5f8964a622a45a397c71336d3d0a4
willieven <102285667+willieven@users.noreply.github.com>
2022-04-05 06:03:59 +0200

    Update production.js

--- config/production.js
+++ config/production.js
@@ -1,1 +1,18 @@
+module.exports = {
+    ftp: {
+        url: 'ftp://0.0.0.0:21',
+        pasv: {
+            ip: '192.168.1.2',
+            portMin: 21000,
+            portMax: 21010,
+        },
+        credentials: {
+            'hikvision': 'Password123'
+        }
+    },
 
+    telegram: {
+        token: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
+        chatIds: [-1337]
+    }
+};
